7da728ac691739dc3aebb1a5281d07c0f96c31ad
Alexandra Eliseeva <klen.sasha_22@list.ru>
2023-04-08 11:28:09 +0300

    stage 8 done?

--- lab_5_scrapper/scrapper.py
+++ lab_5_scrapper/scrapper.py
@@ -1,7 +1,7 @@
 """
 Crawler implementation
 """
-
+import shutil
 from pathlib import Path
 from typing import Pattern, Union
 
@@ -9,13 +9,15 @@ import re
 import json
 from bs4 import BeautifulSoup
 import requests
-from datetime import datetime
+import datetime
 from selenium import webdriver
 from requests import HTTPError
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 import time
 
-
+from core_utils.article.article import Article
+from core_utils.article.io import to_raw, to_meta
+# from core_utils.article.ud import
 from core_utils.config_dto import ConfigDTO
 from core_utils.constants import (ASSETS_PATH, CRAWLER_CONFIG_PATH,
                                   NUM_ARTICLES_UPPER_LIMIT, TIMEOUT_LOWER_LIMIT,
@@ -23,6 +25,9 @@ from core_utils.constants import (ASSETS_PATH, CRAWLER_CONFIG_PATH,
 
 
 class IncorrectSeedURLError(Exception):
+    '''
+
+    '''
     pass
 
 
@@ -78,14 +83,14 @@ class Config:
             config_params = json.load(f)
 
             self.config_dto = ConfigDTO(seed_urls=config_params['seed_urls'],
-                                   headers=config_params['headers'],
-                                   total_articles_to_find_and_parse=
+                                        headers=config_params['headers'],
+                                        total_articles_to_find_and_parse=
                                         config_params['total_articles_to_find_and_parse'],
-                                   encoding=config_params['encoding'],
-                                   timeout=config_params['timeout'],
-                                   should_verify_certificate=
+                                        encoding=config_params['encoding'],
+                                        timeout=config_params['timeout'],
+                                        should_verify_certificate=
                                         config_params['should_verify_certificate'],
-                                   headless_mode=config_params['headless_mode'])
+                                        headless_mode=config_params['headless_mode'])
 
             return self.config_dto
 
@@ -96,9 +101,8 @@ class Config:
         """
         config_dto = self._extract_config_content()
 
-        if not all(re.match('https?://w?w?w?zebra-tv.ru/', url) for url in config_dto.seed_urls):
-            raise IncorrectSeedURLError('seed URL does not match the standard pattern '
-                                        'or does not correspond to the target website')
+        if not all(re.match('https?://w?w?w?.', url) for url in config_dto.seed_urls):
+            raise IncorrectSeedURLError('seed URL does not match the standard pattern')
 
         if config_dto.total_articles not in range(1, NUM_ARTICLES_UPPER_LIMIT):
             raise NumberOfArticlesOutOfRangeError('total number of articles is '
@@ -168,7 +172,6 @@ def make_request(url: str, config: Config) -> requests.models.Response:
     with given configuration
     """
     response = requests.get(url, headers=config.get_headers(),
-                            encoding=config.get_encoding(),
                             timeout=config.get_timeout(),
                             verify=config.get_verify_certificate())
     response.raise_for_status()
@@ -198,8 +201,9 @@ class Crawler:
             url = link['href']
             if url.count('/') == 4 and url[:9] == '/novosti/':
                 url = 'https://www.zebra-tv.ru' + url
-                if url not in self.urls:
+                if url not in self.urls and len(self.urls) < self.config.get_num_articles():
                     self.urls.append(url)
+                    print(url)
 
     def find_articles(self) -> None:
         """
@@ -215,8 +219,7 @@ class Crawler:
 
         scroll_pause_time = 2.5
         last_height = driver.execute_script("return document.body.scrollHeight")
-
-        while len(self.urls) != NUM_ARTICLES_UPPER_LIMIT:
+        while len(self.urls) != self.config.get_num_articles():
             driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
 
             current_html = driver.page_source
@@ -233,7 +236,7 @@ class Crawler:
         """
         Returns seed_urls param
         """
-        return self.urls()
+        return self.urls
 
 
 class HTMLParser:
@@ -241,42 +244,77 @@ class HTMLParser:
     ArticleParser implementation
     """
 
-    def __init__(self, full_url: str, article_id: int, config: Config) -> None:
+    def __init__(self, article_url: str, article_id: int, config: Config) -> None:
         """
         Initializes an instance of the HTMLParser class
         """
-        pass
+        self.article_url = article_url
+        self.article_id = article_id
+        self.config = config
+        self.article = Article(url=self.article_url, article_id=self.article_id)
 
     def _fill_article_with_text(self, article_soup: BeautifulSoup) -> None:
         """
         Finds text of article
         """
-        pass
+        # get the title
+        title = article_soup.find_all('h1', {'class': 'new-title'})[0]
+        title_text = title.text
+
+        # get the preview
+        preview = article_soup.find_all('div', {'class': 'preview-text'})[0]
+        preview_text = preview.text.strip()
+        if preview_text[-1] not in '.?!':
+            preview_text += '.'
+
+        # get the article body
+        body_bs = article_soup.find_all('div', {'class': 'detail'})[0]
+        paragraph_texts = [par.text.strip() for par in body_bs.find_all('p')]
+        article_text = ' '.join(paragraph_texts)
+
+        self.article.title = title_text
+        self.article.text = preview_text + ' ' + article_text
 
     def _fill_article_with_meta_information(self, article_soup: BeautifulSoup) -> None:
         """
         Finds meta information of article
         """
-        pass
+        try:
+            author = article_soup.find_all('span', {'class': 'author'})
+            self.article.author = author[0].text[7:].strip(', ')
+        except IndexError:
+            self.article.author = ['NOT FOUND']
 
-    # def unify_date_format(self, date_str: str) -> datetime.datetime:
-    #     """
-    #     Unifies date format
-    #     """
-    #     pass
+        article_date = article_soup.find(itemprop='datePublished').get('content')
+        article_time = article_soup.find_all('span', {'class': 'date'})[0].text[-6:]
+        date_n_time = self.unify_date_format(article_date + article_time)
+        self.article.date = date_n_time
 
-    # def parse(self) -> Union[Article, bool, list]:
-    #     """
-    #     Parses each article
-    #     """
-    #     pass
+    def unify_date_format(self, date_str: str) -> datetime.datetime:
+        """
+        Unifies date format
+        """
+        return datetime.datetime.strptime(date_str, '%Y-%m-%d %H:%M')
+
+    def parse(self) -> Union[Article, bool, list]:
+        """
+        Parses each article
+        """
+        response = make_request(self.article_url, self.config)
+        article_bs = BeautifulSoup(response.text, 'lxml')
+        self._fill_article_with_text(article_bs)
+        self._fill_article_with_meta_information(article_bs)
+        return self.article
 
 
 def prepare_environment(base_path: Union[Path, str]) -> None:
     """
     Creates ASSETS_PATH folder if no created and removes existing folder
     """
-    base_path.mkdir(exist_ok=True, parents=True)
+    try:
+        base_path.mkdir(parents=True)
+    except FileExistsError:
+        shutil.rmtree(base_path)
 
 
 def main() -> None:
@@ -284,10 +322,17 @@ def main() -> None:
     Entrypoint for scrapper module
     """
     configuration = Config(path_to_config=CRAWLER_CONFIG_PATH)
-    if configuration:
-        prepare_environment(ASSETS_PATH)
-        crawler = Crawler(config=configuration)
-        crawler.find_articles()
+    prepare_environment(ASSETS_PATH)
+    crawler = Crawler(config=configuration)
+    crawler.find_articles()
+    search_urls = crawler.get_search_urls()
+    print(search_urls)
+    for i in range(1, len(search_urls) + 1):
+        parser = HTMLParser(article_url=search_urls[i - 1], article_id=i, config=configuration)
+        parsed_article = parser.parse()
+        to_raw(parsed_article)
+        to_meta(parsed_article)
+        print(i)
 
 
 if __name__ == "__main__":
